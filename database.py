9f478ab1aff671c7d0d4240510f8a365749660a6
Wang1308 <quanghungnamquy@gmail.com>
2025-04-27 21:34:23 +0700

    Update database.py

--- database.py
+++ database.py
@@ -5,6 +5,6 @@ def connect_db():
     return psycopg2.connect(
         host="localhost",
         database="face_attendance",
-        user="postgres",       # đổi nếu user khác
-        password="quang"  # thay bằng mật khẩu của bạn
+        user="postgres",       
+        password="quang"  
     )
